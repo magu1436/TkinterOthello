7927b833f807851ad06034288af93ed3e33a77ce
konoma1103 <2CDI1103.tokai@gmail.com>
2025-06-08 19:05:43 +0900

    DBcontrollerクラスの完成

--- systems.py
+++ systems.py
@@ -8,6 +8,7 @@ from boardgame import Player
 
 
 CONFIG_FILE_PATH = "config.yaml"
+DATABASE_INFO_FILE_PATH = "database_info.yaml"
 
 def load_config() -> dict:
     """config.yamlファイルを読み込み、コンフィグを読み込んで返す関数.
@@ -17,6 +18,16 @@ def load_config() -> dict:
     with open(CONFIG_FILE_PATH, "r") as file:
         config = safe_load(file)
     return config
+
+
+def load_database_info() -> dict:
+    """database_info.yamlファイルを読み込み、情報を読み込んで返す関数.
+    
+    returns:
+        dict: database_info.yamlの内容を保持しているdict"""
+    with open(DATABASE_INFO_FILE_PATH, "r") as file:
+        database_info = safe_load(file)
+    return database_info
 
 
 class Color(Enum):
